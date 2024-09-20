7b9dbe9241233b655d31ee26dd92d27483cddc1d
Andrey Banin <109150204+Mr-Banin-Andrey@users.noreply.github.com>
2024-09-20 22:09:24 +0300

    Initial Commit

---
+++ TestApplicationCanva/TestApplicationCanvaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApplicationCanvaApp.swift
+//  TestApplicationCanva
+//
+//  Created by Андрей Банин on 20.9.24..
+//
+
+import SwiftUI
+
+@main
+struct TestApplicationCanvaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
